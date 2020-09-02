dcb8bddeebb420050484bd8a94023833641e1c78
Edward Knowles <ed.knowles@stunjelly.com>
2020-09-02 09:21:19 +0100

    fix header storybook

--- components/header/header.stories.tsx
+++ components/header/header.stories.tsx
@@ -1,1 +1,34 @@
-import React from 'react';import Header from './header.component';export default {  title: 'Components/Header',  component: Header,};export const Story = () => <Header />;Story.story = {  name: 'header',};
+import React from 'react';
+
+import Header from './header.component';
+
+const Template = (args) => <Header {...args} />;
+
+export default {
+  title: 'Components/Header',
+  component: Header,
+  args: {
+    primaryActiveIndex: 1,
+    links: [
+      {
+        id: 'link4',
+        label: 'Discover',
+        type: 'category',
+        brand: 'discover',
+        linkProps: { href: '/discover' },
+        children: [],
+      },
+      {
+        id: 'link5',
+        label: 'The Charity',
+        type: 'category',
+        brand: 'charity',
+        linkProps: { href: '/charity' },
+        children: [],
+      },
+    ],
+  },
+};
+
+export const Story = Template.bind({});
+Story.args = {};
