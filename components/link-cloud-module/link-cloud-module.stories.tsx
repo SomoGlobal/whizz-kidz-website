ac65fb7b1deba3ab15c07a88acefee05e5e30518
Edward Knowles <ed.knowles@stunjelly.com>
2020-10-02 11:18:09 +0100

    fix link cloud module story

--- components/link-cloud-module/link-cloud-module.stories.tsx
+++ components/link-cloud-module/link-cloud-module.stories.tsx
@@ -1,1 +1,42 @@
-import React from 'react';import LinkCloudModule from './link-cloud-module.component';const Template = (args) => <LinkCloudModule {...args} />;export default {  title: 'Components/LinkCloudModule',  component: LinkCloudModule,};export const Story = Template.bind({});Story.args = {};
+import React from 'react';
+
+import LinkCloudModule from './link-cloud-module.component';
+
+const Template = (args) => <LinkCloudModule {...args} />;
+
+export default {
+  title: 'Components/LinkCloudModule',
+  component: LinkCloudModule,
+  args: {
+    items: [
+      {
+        name: 'North England',
+        slug: 'north-england',
+        _modelApiKey: 'region',
+      },
+      {
+        name: 'London',
+        slug: 'london',
+        _modelApiKey: 'region',
+      },
+      {
+        name: 'South England',
+        slug: 'south-england',
+        _modelApiKey: 'region',
+      },
+      {
+        name: 'Scotland',
+        slug: 'scotland',
+        _modelApiKey: 'region',
+      },
+      {
+        name: 'Wales',
+        slug: 'wales',
+        _modelApiKey: 'region',
+      },
+    ],
+  },
+};
+
+export const Story = Template.bind({});
+Story.args = {};
