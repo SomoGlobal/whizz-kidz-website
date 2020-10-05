b75268c2b7b911397760ce13013849cfef072098
Edward Knowles <ed.knowles@stunjelly.com>
2020-10-05 09:43:38 +0100

    fix topic tag border radius on mobile

--- components/topic-tag/topic-tag.component.tsx
+++ components/topic-tag/topic-tag.component.tsx
@@ -10,7 +10,7 @@ const TopicTag: React.FC<ITopicTagProps> = ({ name, slug }) => {
   return (
     <Link href="/discover/topic/[slug]" as={`/discover/topic/${slug}`}>
       <a
-        className="px-3 py-1 text-sm font-medium text-indigo-900 bg-gray-300 sm:rounded-full hover:underline"
+        className="px-3 py-1 text-sm font-medium text-indigo-900 bg-gray-300 rounded-full hover:underline"
         aria-label={`Topic: ${name}`}
       >
         {name}
