703f0e39a35469b7e4456f26180f0ffec405b3cc
Edward Knowles <ed.knowles@stunjelly.com>
2020-10-05 09:44:33 +0100

    fix mobile dialog opacity

--- components/modal-dialog/modal-dialog.component.tsx
+++ components/modal-dialog/modal-dialog.component.tsx
@@ -35,31 +35,30 @@ const ModalDialog: React.FC<IModalDialogProps> = (props) => {
 
   return (
     <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
+      initial={{ background: 'rgba(0, 0, 0, 0)' }}
+      animate={{ background: 'rgba(0, 0, 0, 0.5)' }}
+      exit={{ background: 'rgba(0, 0, 0, 0)' }}
       className={cx('fixed flex top-0 left-0 bottom-0 right-0', {
         'items-center justify-center': position === 'center',
         'items-stretch justify-end': position === 'right-full',
       })}
       style={{
         zIndex: 100,
-        background: 'rgba(0, 0, 0, 0.5)',
       }}
     >
       <FocusScope contain restoreFocus autoFocus>
         <motion.div
           transition={{ ease: 'circOut' }}
-          initial={{ opacity: 0, x: shouldReduceMotion ? 0 : '100%' }}
-          animate={{ opacity: 1, x: '0%' }}
-          exit={{ opacity: 0, x: shouldReduceMotion ? 0 : '100%' }}
+          initial={{ x: shouldReduceMotion ? 0 : '100%' }}
+          animate={{ x: '0%' }}
+          exit={{ x: shouldReduceMotion ? 0 : '100%' }}
           {...(overlayProps as any)}
           {...(dialogProps as any)}
           {...(modalProps as any)}
           ref={ref}
           className="bg-white max-w-3xl w-full flex flex-col"
         >
-          <div className="z-50 flex items-center justify-end px-4 py-2 text-gray-800 bg-white">
+          <div className="z-50 flex items-center justify-end px-4 text-gray-800 bg-white">
             <h1
               {...titleProps}
               className="inline px-4 py-3 text-3xl font-bold clone flex-1"
