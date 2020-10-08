463f12245edd632bfee16421635b5e208ee0b1b1
Edward Knowles <ed.knowles@stunjelly.com>
2020-10-08 12:27:03 +0100

    add svg decorations

--- components/decoration/decoration.component.tsx
+++ components/decoration/decoration.component.tsx
@@ -1,4 +1,8 @@
+import { useReducedMotion } from 'framer-motion';
 import React from 'react';
+import { Parallax } from 'react-scroll-parallax/cjs';
+import cx from 'classnames';
+import Container from '../container';
 import DecorateDiamond from '../decorate-diamond';
 import DecorateTriangle from '../decorate-triangle';
 
@@ -7,10 +11,35 @@ export interface IDecorationProps {
   decorationPosition: string;
 }
 
+const SVGDecoration = ({ fileName, disableParallax, position }) => (
+  <Container>
+    <Parallax
+      disabled={disableParallax}
+      y={[-10, 10]}
+      x={[-10, 10]}
+      className={cx('absolute -z-1', {
+        'left-0': position === 'left',
+        'right-0': position === 'right',
+      })}
+    >
+      <img
+        alt="Decoration"
+        src={`/svg/decoration/${fileName}.svg`}
+        className={cx('transform -translate-y-1/2', {
+          '-translate-x-1/2': position === 'left',
+          'translate-x-1/2': position === 'right',
+        })}
+      />
+    </Parallax>
+  </Container>
+);
+
 const Decoration: React.FC<IDecorationProps> = ({
   decorationType,
   decorationPosition,
 }) => {
+  const shouldReduceMotion = useReducedMotion();
+
   return (
     <div role="presentation">
       {decorationType === 'diamond' && (
@@ -19,8 +48,12 @@ const Decoration: React.FC<IDecorationProps> = ({
       {decorationType === 'triangle' && (
         <DecorateTriangle position={decorationPosition as any} />
       )}
-      {decorationType === 'doughnut' && (
-        <img src="/svg/decoration/doughnut01.svg" alt={decorationType} />
+      {decorationType !== 'triangle' && decorationType !== 'diamond' && (
+        <SVGDecoration
+          position={decorationPosition}
+          disableParallax={shouldReduceMotion}
+          fileName={decorationType}
+        />
       )}
     </div>
   );
