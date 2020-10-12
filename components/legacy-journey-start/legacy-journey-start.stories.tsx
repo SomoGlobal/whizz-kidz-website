657dbc65ddcb9b7e8fbd9bd360d76767e6e5756c
Edward Knowles <ed.knowles@stunjelly.com>
2020-10-12 10:48:47 +0100

    fix storybook build

--- components/legacy-journey-start/legacy-journey-start.stories.tsx
+++ components/legacy-journey-start/legacy-journey-start.stories.tsx
@@ -1,1 +1,19 @@
-import React from 'react';import LegacyJourneyStart from './legacy-journey-start.component';const Template = (args) => <LegacyJourneyStart {...args} />;export default {  title: 'Components/LegacyJourneyStart',  component: LegacyJourneyStart,};export const Story = Template.bind({});Story.args = {};
+import React from 'react';
+import { brands } from '../../lib/brand-context';
+
+import LegacyJourneyStart from './legacy-journey-start.component';
+
+const Template = (args) => <LegacyJourneyStart {...args} />;
+
+export default {
+  title: 'Components/LegacyJourneyStart',
+  component: LegacyJourneyStart,
+  args: {
+    href: '/kidz',
+    label: 'Kidz',
+    brand: brands.kidz,
+  },
+};
+
+export const Story = Template.bind({});
+Story.args = {};
