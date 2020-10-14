e10803de7c5328956a8c6b95335f64a83cc7b2a1
Edward Knowles <ed.knowles@stunjelly.com>
2020-10-14 11:32:00 +0100

    add google analytics tag to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,23 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-16130012-5"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'UA-16130012-5');
+            `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
