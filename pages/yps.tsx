125e04fe0ffb8fd0b185e99fa23cdaae45ed4b63
Edward Knowles <ed.knowles@stunjelly.com>
2020-10-19 10:31:35 +0100

    add yps page

---
+++ pages/yps.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import React from 'react';
+import Layout from '../components/layout';
+
+const YPS: React.FC = () => (
+  <Layout brand="home">
+    <Head>
+      <title>Young People's Services | Whizz-Kidz</title>
+      <meta name="robots" content="noindex" />
+    </Head>
+    <iframe
+      id="yps"
+      title="Young People's Services"
+      onLoad={() => window.parent.scrollTo(0, 0)}
+      allowTransparency
+      allowFullScreen
+      allow="geolocation; microphone; camera"
+      src="https://www.whizz-kidz-formz.co.uk/"
+      frameBorder="0"
+      className="w-full h-full min-h-screen"
+    />
+  </Layout>
+);
+
+export default YPS;
