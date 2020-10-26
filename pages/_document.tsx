5a605b38f4646e825342bcaad56bd4fe755830c9
Edward Knowles <ed.knowles@stunjelly.com>
2020-10-26 11:30:55 +0000

    add basic fb pixel

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,8 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 import { GA_TRACKING_ID } from '../lib/google-analytics';
 
+const FB_APP_ID = '791640358066511';
+
 export default class MyDocument extends Document<{ isProduction: boolean }> {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -40,6 +42,33 @@ export default class MyDocument extends Document<{ isProduction: boolean }> {
                   `,
                 }}
               />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '${FB_APP_ID}');
+                    fbq('track', 'PageView');
+                  `,
+                }}
+              />
+              <noscript>
+                <img
+                  height="1"
+                  width="1"
+                  className="hidden"
+                  style={{ display: 'none' }}
+                  alt=""
+                  role="presentation"
+                  src={`https://www.facebook.com/tr?id=${FB_APP_ID}&ev=PageView&noscript=1`}
+                />
+              </noscript>
             </>
           )}
         </Head>
