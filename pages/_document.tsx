c4c1d88e254a71057d459122b027da09cc5e8b80
Hader Cardona <hadermania@gmail.com>
2021-03-15 03:53:44 -0500

    add google analytics 4

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,15 +20,21 @@ export default class MyDocument extends Document<{ isProduction: boolean }> {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{
               __html: `
-                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','${GTM_ID}');
-                  `,
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${GA_TRACKING_ID}');
+              `,
             }}
           />
         </Head>
